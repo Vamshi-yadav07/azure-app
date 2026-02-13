71be3b14859858b4b67dd98d36ccba1ea5719866
sripathi vamshi <154038542+Vamshi-yadav07@users.noreply.github.com>
2026-02-14 01:07:20 +0530

    Revert "Set output configuration to standalone in next.config.ts"
    This reverts commit a87adcc05dc1def167d3390e870a2cbb8daffcd6.

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone',        // <--- enable standalone build
-  poweredByHeader: false,      // recommended for security
-  compress: false,             // disable built-in compression (Azure handles it)
-  reactStrictMode: true 
+  /* config options here */
 };
 
 export default nextConfig;
